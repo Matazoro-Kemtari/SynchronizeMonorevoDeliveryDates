a086ef14acca5437b970518569d8e42a220db81e
kondara-mach <k_hirano@wadass.com>
2022-09-06 10:55:41 +0900

    ログローテーションを追加

--- go-build-raspberrypi.ps1
+++ go-build-raspberrypi.ps1
@@ -2,4 +2,4 @@ $env:GOOS="linux"
 $env:GOARCH="arm"
 $env:GOARM="6"
 
-go build -ldflags "-X main.version=v1.2.2 -X main.revision=$(git rev-parse --short HEAD)" .
+go build -ldflags "-X main.version=v1.0.0 -X main.revision=$(git rev-parse --short HEAD)" .
